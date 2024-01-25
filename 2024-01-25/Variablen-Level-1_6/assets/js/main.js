32bd4d0bcbefb8e03f8406b4cec2118325b3166e
ChrissQAng <goodomen@outlook.de>
2024-01-25 13:46:10 +0100

    intro done

---
+++ 2024-01-25/Variablen-Level-1_6/assets/js/main.js
@@ -0,0 +1,11 @@
+// let x = 34;
+// x = 67;
+// console.log(x);
+
+const y = 34;
+y = 67;
+console.log(y);
+
+// y ist eine Konstante und kann nicht verändert werden!
+
+// Assignment to constant variable.
