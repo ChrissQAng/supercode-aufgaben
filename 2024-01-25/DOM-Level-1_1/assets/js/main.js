39c3f2dd0e2502f24569b1b9084a1c3976ba6861
ChrissQAng <goodomen@outlook.de>
2024-01-25 14:48:27 +0100

    doc write done

---
+++ 2024-01-25/DOM-Level-1_1/assets/js/main.js
@@ -0,0 +1,17 @@
+document.write("<h1>" + "Hello World" + "</h1>");
+document.write("<p>" + "Have a nice day" + "</p>");
+
+const myText = "Hello again.";
+document.write(myText);
+
+let vorname = "Chris";
+let nachname = "Christopherson";
+document.write(
+  "<h2>" +
+    "Hi, ich bin " +
+    vorname +
+    " " +
+    nachname +
+    " SuperCoder!!!" +
+    "</h2>"
+);
