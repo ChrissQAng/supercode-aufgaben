0aa9fe847e8acf3a4b95940b88eba7ab6acbb3ea
ChrissQAng <goodomen@outlook.de>
2024-01-25 14:36:15 +0100

    arithmetic operants done

---
+++ 2024-01-25/Intro-Level-1_12/assets/js/main.js
@@ -0,0 +1,20 @@
+let score = 5 + 5 * 10;
+console.log("Ergebnis:" + score);
+
+score = (5 + 5) * 10;
+console.log("result" + score);
+
+score = 0;
+score = score + 10;
+console.log("score:" + score);
+
+score += 10;
+console.log("score:" + score);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+zahl++;
+zahl--;
+console.log("increment" + zahl);
