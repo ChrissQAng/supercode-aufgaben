6113070b0038669ffa0504fd5bc0c232acdfc357
ChrissQAng <goodomen@outlook.de>
2024-01-25 13:30:54 +0100

    Console-Level-1_1 done

---
+++ 2024-01-25/Console-Level-1_1/assets/js/main.js
@@ -0,0 +1,44 @@
+// This is a single line JavaScript comment
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+//  Console-Level-1_1
+
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+console.log("Hello World");
+
+const firstName = "Anton";
+const age = 28;
+console.log(firstName, age);
+
+const job = "Trainer:in";
+console.log(job);
+
+let married = true;
+console.log(married);
+
+const satz =
+  firstName +
+  " ist " +
+  age +
+  " Jahre alt, von Beruf " +
+  job +
+  " und ist verheiratet: " +
+  married +
+  ".";
+console.log(satz);
+
+document.querySelector("#output").innerHTML = b;
