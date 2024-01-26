50f73206193897b6281794884da41421a327a2c7
ChrissQAng <goodomen@outlook.de>
2024-01-26 15:05:49 +0100

    26 01 2024 done

---
+++ 2024-01-26/Functions-Level-1_5/assets/js/main.js
@@ -0,0 +1,14 @@
+function math(x, y) {
+  let multiplication = x * y;
+  let division = x / y;
+
+  console.log(x + " mal " + y + "ergibt: " + multiplication);
+  console.log(x + " geteilt durch " + y + " ergibt: " + division);
+}
+
+math(10, 2);
+math(30, 20);
+math(100, 100);
+math(5, 0);
+math(45, 173);
+math(1, 1000);
