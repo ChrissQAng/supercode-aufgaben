fc76925ae2d3e2d0f795373541ca20592c100434
ChrissQAng <goodomen@outlook.de>
2024-01-26 14:12:40 +0100

    innerHTML done

---
+++ 2024-01-26/DOM-Level-1_3/assets/js/main.js
@@ -0,0 +1,3 @@
+let images = document.getElementById("gallery");
+images.innerHTML =
+  "<figure><img src='assets/img/funny dog.avif' width='50%' <figcaption>Fig.1</figcaption></figure><figure><img src='assets/img/funny dog.avif' width='50%' <figcaption>Fig.2</figcaption></figure><figure><img src='assets/img/funny dog.avif' width='50%' <figcaption>Fig.3</figcaption></figure>";
