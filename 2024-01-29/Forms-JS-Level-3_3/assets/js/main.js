b141a43b3042638dd0b6d91ae9295475bcc6af64
ChrissQAng <goodomen@outlook.de>
2024-01-29 15:46:25 +0100

    added values of color

--- 2024-01-29/Forms-JS-Level-3_3/assets/js/main.js
+++ 2024-01-29/Forms-JS-Level-3_3/assets/js/main.js
@@ -5,4 +5,11 @@ function changeBackground() {
   const valueRGB = `rgb(${red},${green},${blue})`;
   const wrapper = document.querySelector(".wrapper");
   wrapper.style.backgroundColor = valueRGB;
+  // implement values
+  const valueRed = document.querySelector(".valueRed");
+  valueRed.innerHTML = red;
+  const valueGreen = document.querySelector(".valueGreen");
+  valueGreen.innerHTML = green;
+  const valueBlue = document.querySelector(".valueBlue");
+  valueBlue.innerHTML = blue;
 }
