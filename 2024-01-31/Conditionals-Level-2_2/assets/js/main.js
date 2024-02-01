9fc794884dc0b72775341fd5590e0a4fade90333
ChrissQAng <goodomen@outlook.de>
2024-02-01 09:53:57 +0100

    textContent dazu

--- 2024-01-31/Conditionals-Level-2_2/assets/js/main.js
+++ 2024-01-31/Conditionals-Level-2_2/assets/js/main.js
@@ -1,25 +1,30 @@
 function changeAir() {
-  const air = document.querySelector("#air").value;
+  const air = Number(document.querySelector("#air").value);
   //   --- output: air-value
   const wrapper = document.querySelector(".wrapper");
   const airValue = document.querySelector(".value-air");
-  airValue.innerHTML = air;
+  // was NEUES!!!
+  airValue.textContent = "Luftqualität: " + air;
+  // airValue.innerHTML = air;
+
   //   --- output level
   const healthConcern = document.querySelector(".health-concern");
   const healthEffect = document.querySelector(".heath-effect");
   if (air <= 50) {
     wrapper.style.backgroundColor = "green";
-    healthConcern.innerHTML = "Level of health concern: Good";
-    healthEffect.innerHTML = "Level of health effects: Little or no risk";
-  } else if (air <= 100) {
+    healthConcern.textContent = "Level of health concern: Good";
+    healthEffect.textContent = "Level of health effects: Little or no risk";
+  } else if (air <= 100 && air > 50) {
     wrapper.style.backgroundColor = "yellow";
-    healthConcern.innerHTML = "Level of health concern: Moderate";
-    healthEffect.innerHTML = "Level of health effects: Acceptable quality";
-  } else {
+    healthConcern.textContent = "Level of health concern: Moderate";
+    healthEffect.textContent = "Level of health effects: Acceptable quality";
+  } else if (air > 100 && air <= 150) {
     wrapper.style.backgroundColor = "orange";
-    healthConcern.innerHTML =
+    healthConcern.textContent =
       "Level of health concern: Unhealthy for sensitive groups";
-    healthEffect.innerHTML =
+    healthEffect.textContent =
       "Level of health effects: Generable publics not likely affected";
+  } else {
+    console.log("Fehler, Wert nicht innerhalb der Range!");
   }
 }
