0ecd89882acbe19492eb40f80e77f3786ca35b1b
ChrissQAng <goodomen@outlook.de>
2024-02-01 19:31:36 +0100

    really really

--- 2024-02-01/Projects-JS-Einführung-Level-2_1/assets/js/main.js
+++ 2024-02-01/Projects-JS-Einführung-Level-2_1/assets/js/main.js
@@ -2,8 +2,7 @@ function text() {
   const textInput = document.body.querySelector(".text-input").value;
   const showText = document.body.querySelector(".text-output");
   const noText = document.body.querySelector(".no-message");
-  const textOuput =
-    textInput.length != 0
-      ? (showText.textContent = textInput)
-      : (noText.textContent = "Gib bitte eine Nachricht ein!");
+  showText.textContent = textInput;
+  noText.textContent =
+    textInput.length != 0 ? "" : "Gib bitte eine Nachricht ein!";
 }
