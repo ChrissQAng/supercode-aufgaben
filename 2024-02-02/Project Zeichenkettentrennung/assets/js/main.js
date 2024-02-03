98f2b769e2a9282c35128e96d9b3d1e69d97465c
ChrissQAng <goodomen@outlook.de>
2024-02-03 18:27:06 +0100

    false alert included

--- 2024-02-02/Project Zeichenkettentrennung/assets/js/main.js
+++ 2024-02-02/Project Zeichenkettentrennung/assets/js/main.js
@@ -56,17 +56,26 @@ function slice() {
   const outputSlice1 = document.body.querySelector(".output-slice1");
   const outputSlice2 = document.body.querySelector(".output-slice2");
 
-  // --- index of slice start
+  // --- false input validation
+  if (completeText === "" || sliceString === "") {
+    outputSlice1.textContent = "Gib in die Felder jeweils einen Text ein!";
+    return;
+  }
 
+  // --- index of slice start
   const indexSliceStringStart = completeText.indexOf(sliceString);
   console.log(indexSliceStringStart);
 
-  // --- index of slice string end
+  if (indexSliceStringStart === -1) {
+    outputSlice1.textContent =
+      "Trennung ist NICHT in der ursprünglichen Zeichenkette enthalte!";
+    return;
+  }
 
+  // --- index of slice string end
   const indexSliceStringEnd = indexSliceStringStart + sliceString.length;
 
   // --- variable für slice1 und slice2 Before & After
-
   const slice1Before = completeText.slice(0, indexSliceStringStart);
   const slice2Before = completeText.slice(indexSliceStringStart);
 
@@ -75,18 +84,6 @@ function slice() {
     indexSliceStringEnd,
     completeText.length
   );
-  //   output of 4 variables
-
-  // if (beforeBtn) {
-  //   console.log("before");
-  //   outputSlice1.textContent = slice1Before;
-  //   outputSlice2.textContent = slice2Before;
-  // }
-  // else {
-  //   outputSlice1.textContent = slice1After;
-  //   outputSlice2.textContent = slice2After;
-  //   console.log("after");
-  // }
   outputSlice1.textContent = beforeBtn ? slice1Before : slice1After;
   outputSlice2.textContent = beforeBtn ? slice2Before : slice2After;
 }
