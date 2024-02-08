24adc9fbdd329851dda62310856b796065c774b3
ChrissQAng <goodomen@outlook.de>
2024-02-08 16:45:27 +0100

    alaaf

--- 2024-02-08/Array-Iteration-Level-1_6/assets/js/main.js
+++ 2024-02-08/Array-Iteration-Level-1_6/assets/js/main.js
@@ -5,6 +5,13 @@ let album = [
   "rooms.GIF",
   "DOGATBEACH.jpg",
 ];
-let array = album.map((element) => {
-    
-})
+let array = album.map((items) => {
+  let element = items.toLowerCase();
+  if (element.includes(".gif") || element.includes(".jpg")) {
+    return element.slice(0, -4);
+  } else {
+    return (element = "invalid");
+  }
+});
+
+console.log(array);
