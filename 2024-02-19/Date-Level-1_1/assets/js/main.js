92b5fbf830c9e26e7198a36b639334c42e38e349
ChrissQAng <goodomen@outlook.de>
2024-02-19 17:29:59 +0100

    now with clock

--- 2024-02-19/Date-Level-1_1/assets/js/main.js
+++ 2024-02-19/Date-Level-1_1/assets/js/main.js
@@ -4,8 +4,10 @@ const date1 = new Date("September 2, 2019 09:00:00");
 const date2 = new Date(0);
 const date3 = new Date(31556908800);
 const date4 = new Date(86400000);
+const now = new Date();
 
 output.innerHTML = `${date1} = new Date("September 2, 2019 09:00:00")</br>`;
 output.innerHTML += `${date2} = new Date(0)</br>`;
 output.innerHTML += `${date3} = new Date(31556908800)</br>`;
 output.innerHTML += `${date4} = new Date(86400000)</br>`;
+output.innerHTML += `${now.toUTCString()} = new Date()</br>`;
--- 2024-02-19/Date-Level-1_2/assets/js/main.js
+++ 2024-02-19/Date-Level-1_2/assets/js/main.js
@@ -1,42 +1,62 @@
-let monate = [
-  "Januar",
-  "Februar",
-  "März",
-  "April",
-  "Mai",
-  "Juni",
-  "Juli",
-  "August",
-  "September",
-  "Oktober",
-  "November",
-  "Dezember",
-];
+// let monate = [
+//   "Januar",
+//   "Februar",
+//   "März",
+//   "April",
+//   "Mai",
+//   "Juni",
+//   "Juli",
+//   "August",
+//   "September",
+//   "Oktober",
+//   "November",
+//   "Dezember",
+// ];
 
-let wochenTag = [
-  "Sonntag",
-  "Montag",
-  "Dienstag",
-  "Mittwoch",
-  "Donnerstag",
-  "Freitag",
-  "Samstag",
-];
-const output = document.querySelector(".output-time");
-const actualTime = new Date();
+// let wochenTag = [
+//   "Sonntag",
+//   "Montag",
+//   "Dienstag",
+//   "Mittwoch",
+//   "Donnerstag",
+//   "Freitag",
+//   "Samstag",
+// ];
+// const output = document.querySelector(".output-time");
+// const actualTime = new Date();
 
-output.innerHTML += `${actualTime} </br>`;
-output.innerHTML += `${actualTime.getFullYear()}</br>`;
-output.innerHTML += `${actualTime.getMonth()} Monat</br>`;
-output.innerHTML += `${actualTime.getHours()} Tag</br>`;
-output.innerHTML += `${actualTime.getMinutes()} Minute</br>`;
-wochenTag.forEach((tag) => {
-  output.innerHTML +=
-    actualTime.toLocaleDateString("default", {
-      weekday: "long",
-    }) === tag
-      ? `<p>${tag}</p>`
-      : "";
-});
+// output.innerHTML += `${actualTime} </br>`;
+// output.innerHTML += `${actualTime.getFullYear()}</br>`;
+// output.innerHTML += `${actualTime.getMonth()} Monat</br>`;
+// output.innerHTML += `${actualTime.getHours()} Tag</br>`;
+// output.innerHTML += `${actualTime.getMinutes()} Minute</br>`;
+// wochenTag.forEach((tag) => {
+//   output.innerHTML +=
+//     actualTime.toLocaleDateString("default", {
+//       weekday: "long",
+//     }) === tag
+//       ? `<p>${tag}</p>`
+//       : "";
+// });
 
-output.innerHTML += monate[actualTime.getMonth()];
+// output.innerHTML += monate[actualTime.getMonth()];
+
+// --- Uhr
+// const outDay = document.querySelector(".day");
+
+const outputTime = () => {
+  const currentTime = new Date();
+  document.querySelector(".day").textContent = currentTime.toLocaleString(
+    "default",
+    {
+      weekday: "short",
+    }
+  );
+  document.querySelector(".hour").textContent = currentTime.getHours() - 12;
+  document.querySelector(".minute").textContent = currentTime.getMinutes();
+  document.querySelector(".second").textContent = currentTime.getSeconds();
+  document.querySelector(".noon").textContent =
+    currentTime.getSeconds() + 12 >= 12 ? "PM" : "AM";
+};
+
+setInterval(outputTime, 1000);
