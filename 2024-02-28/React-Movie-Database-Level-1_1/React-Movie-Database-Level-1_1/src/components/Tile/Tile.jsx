29e97bffbbee94ddff82f45f1af61a4737342378
ChrissQAng <goodomen@outlook.de>
2024-02-28 17:19:45 +0100

    push

--- 2024-02-28/React-Movie-Database-Level-1_1/React-Movie-Database-Level-1_1/src/components/Tile/Tile.jsx
+++ 2024-02-28/React-Movie-Database-Level-1_1/React-Movie-Database-Level-1_1/src/components/Tile/Tile.jsx
@@ -1,13 +1,19 @@
 import "./Tile.css";
 const Tile = (props) => {
-  console.log(props);
   return (
     <div className="tile">
-      <p>{props.content.title}</p>
+      <p className="css-title">{props.content.title}</p>
       <p>{props.content.year}</p>
       <p>{props.content.director}</p>
       <p>{props.content.duration}</p>
-      <p>{props.content.genre}</p>
+      <p>{props.content.rate}</p>
+      <div className="wrapper-genre">
+        {props.content.genre.map((genre, index) => (
+          <p className="css-genre" key={index}>
+            {genre}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
