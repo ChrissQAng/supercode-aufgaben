9af9466b6e30ff9dfe2a5c4a7c0a0628ebe5b6e1
ChrissQAng <goodomen@outlook.de>
2024-04-09 14:23:14 +0200

    server with background image

---
+++ 2024-04-09/http-Level-1_1_Level-2_1/index.js
@@ -0,0 +1,86 @@
+const http = require("http");
+const { readFile } = require("./filesystem.js");
+
+// routing
+
+const server = http.createServer((request, response) => {
+  if (
+    request.method === "GET" &&
+    (request.url === "/" || request.url === "/home")
+  ) {
+    readFile("./pages/home.html")
+      .then((dataBuffer) => {
+        response.write(dataBuffer);
+        response.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        response.end("Internal Server Error!");
+      });
+  } else if (request.method === "GET" && request.url === "/about") {
+    readFile("./pages/about.html")
+      .then((dataBuffer) => {
+        response.write(dataBuffer);
+        response.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        response.end("Internal Server Error!");
+      });
+  } else if (request.method === "GET" && request.url === "/contact") {
+    readFile("./pages/contact.html")
+      .then((dataBuffer) => {
+        response.write(dataBuffer);
+        response.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        response.end("Internal Server Error!");
+      });
+  } else if (request.method === "GET" && request.url === "/faq") {
+    readFile("./pages/faq.html")
+      .then((dataBuffer) => {
+        response.write(dataBuffer);
+        response.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        response.end("Internal Server Error!");
+      });
+  } else if (request.method === "GET" && request.url === "/stylescss") {
+    readFile("./css/styles.css")
+      .then((dataBuffer) => {
+        response.write(dataBuffer);
+        response.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        response.end("Internal Server Error!");
+      });
+  } else if (request.method === "GET" && request.url === "/images/forest.jpg") {
+    readFile("./images/forest.jpg")
+      .then((dataBuffer) => {
+        response.write(dataBuffer);
+        response.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        response.end("Internal Server Error!");
+      });
+  } else {
+    readFile("./pages/error.html")
+      .then((dataBuffer) => {
+        response.write(dataBuffer);
+        response.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        response.end("Internal Server Error!");
+      });
+  }
+});
+
+// requestListener & port 3003
+
+const PORT = 3003;
+server.listen(PORT, () => console.log("server ready PORT", PORT));
