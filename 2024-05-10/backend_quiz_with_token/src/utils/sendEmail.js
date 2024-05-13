e87010ea84559ab3c60ea70d22a547f158bab153
ChrissQAng <goodomen@outlook.de>
2024-05-13 12:50:42 +0200

    nodemailer variablen sind in der .env

---
+++ 2024-05-10/backend_quiz_with_token/src/utils/sendEmail.js
@@ -0,0 +1,5 @@
+const GMAIL_ADRESS = process.env.GMAIL_ADRESS;
+const CLIENT_ID = process.env.GMAIL_CLIENT_ID;
+const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET;
+const REDIRECT_URI = process.env.GMAIL_REDIRECT_URI;
+const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
